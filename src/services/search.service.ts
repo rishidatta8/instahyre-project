21f6c477b5649a5e66dfb663d72fb90ffea2d21d
rishi-fanclash <rishi_moolya@fanclash.in>
2024-12-12 17:19:44 +0530

    updated sorting logic and added contacts support

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -17,19 +17,31 @@ export class SearchService {
   ) {}
 
   async searchByName(query: string) {
+    const loggedInUser: User = this.userContext.getCurrentUser();
     const users: User[] = await this.userRepository.find({
+      where: { name: ILike(`%${query}%`) },
+      order: { name: 'ASC' },
+    });
+    const contacts: Contact[] = await this.contactRepository.find({
       where: { name: ILike(`%${query}%`) },
       order: { name: 'ASC' },
     });
 
     // Sort users to prioritize exact matches
-    return users
+    return [...users, ...contacts]
       .sort((a, b) => {
-        if (a.name.toLowerCase() === query.toLowerCase()) return -1;
-        if (b.name.toLowerCase() === query.toLowerCase()) return 1;
+        const startsWithA = (name) =>
+          name.toLowerCase().startsWith(query.toLowerCase());
+        if (startsWithA(a.name) && !startsWithA(b.name)) return -1;
+        if (!startsWithA(a.name) && startsWithA(b.name)) return 1;
         return a.name.localeCompare(b.name);
       })
-      .map((user: User) => Utils.mapToUserResponsePayload(user));
+      .map((user: User) =>
+        Utils.mapToUserResponsePayload(
+          user,
+          this.includeEmail(loggedInUser, user),
+        ),
+      );
   }
 
   async searchByPhoneNumber(phoneNumber: string) {
@@ -38,9 +50,7 @@ export class SearchService {
       where: { phoneNumber, id: Not(loggedInUser.id) },
     });
     if (user) {
-      const includeEmail: boolean = loggedInUser.contacts.some(
-        (contact) => contact.phoneNumber === user.phoneNumber,
-      );
+      const includeEmail: boolean = this.includeEmail(loggedInUser, user);
       return Utils.mapToUserResponsePayload(user, includeEmail);
     }
     const results = await this.contactRepository.find({
@@ -50,5 +60,11 @@ export class SearchService {
       },
     });
     return results.map((contact) => Utils.mapToUserResponsePayload(contact));
+  }
+
+  private includeEmail(loggedInUser: User, userToCheck: User): boolean {
+    return loggedInUser.contacts.some(
+      (contact) => contact.phoneNumber === userToCheck.phoneNumber,
+    );
   }
 }
